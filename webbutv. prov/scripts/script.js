aa58ca6b5c16a82bdf82fbacc7cecaaf9dd3c8f0
KRILLE12345 <96288433+KRILLE12345@users.noreply.github.com>
2022-05-20 10:09:28 +0200

    Delete webbutv. prov directory

--- webbutv. prov/scripts/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#generateCard").addEventListener("click", function(e) {
-    let picture = document.querySelector("#cardPic").value
-    let text = document.querySelector("#cardText").value
-    let color = document.querySelector("#colorPicker").value
-    let generatedCard = document.querySelector("#generatedCard")
-
-    generatedCard.innerHTML = `${text}<img src = "img/${picture}">`
-    generatedCard.style.background = color;
-})
